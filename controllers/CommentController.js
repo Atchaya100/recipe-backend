90b7c5addf6f83a4e75f4f25a71f916273277c9e
Atchaya100 <atchaya00@gmail.com>
2023-12-13 16:08:34 +0530

    adding comment section

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -21,9 +21,8 @@ const createComment=async(req,res)=>{
             }
         })
 }
-const getComments=async(res,req)=>{
+const getComments=async(req,res)=>{
     const id=req.params.id
-    
     try{   
         const u=await db.findOne({rid:id});
         res.status(200).send(u);
